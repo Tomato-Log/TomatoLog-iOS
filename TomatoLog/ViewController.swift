b59cbb1df6790f45a75106c16d8a10aeb9e81f99
wookeon <creator_wookeon@naver.com>
2021-04-11 17:41:05 +0900

    프로젝트 생성

---
+++ TomatoLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TomatoLog
+//
+//  Created by wookeon on 2021/04/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
