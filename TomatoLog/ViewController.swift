a856f3936f279ac66e240e5efbbd0c0a06b0c10c
wookeon <creator_wookeon@naver.com>
2021-04-11 20:38:53 +0900

    불필요한 주석 제거

--- TomatoLog/ViewController.swift
+++ TomatoLog/ViewController.swift
@@ -1,19 +1,11 @@
-//
-//  ViewController.swift
-//  TomatoLog
-//
-//  Created by wookeon on 2021/04/11.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
-
