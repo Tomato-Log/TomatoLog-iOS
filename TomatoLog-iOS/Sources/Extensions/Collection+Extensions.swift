db48ce4f23dc5b873761ce5f4baea42d60c1c0a0
wookeon <creator_wookeon@naver.com>
2021-05-15 23:41:35 +0900

    Add Collection+Extensions

---
+++ TomatoLog-iOS/Sources/Extensions/Collection+Extensions.swift
@@ -0,0 +1,10 @@
+
+import Foundation
+
+
+extension Collection {
+
+    subscript (safe index: Index) -> Iterator.Element? {
+        return index >= startIndex && index < endIndex ? self[index] : nil
+    }
+}
