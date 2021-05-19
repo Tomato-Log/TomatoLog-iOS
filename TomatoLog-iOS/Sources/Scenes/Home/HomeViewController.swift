9127d3f786a49304da4693ce900f367b2dd89d0d
yujin <glisteneugene@gmail.com>
2021-05-19 14:24:57 +0900

    UI 설계

--- TomatoLog-iOS/Sources/Scenes/Home/HomeViewController.swift
+++ TomatoLog-iOS/Sources/Scenes/Home/HomeViewController.swift
@@ -1,36 +1,54 @@
+//
+//  HomeViewController.swift
+//  TomatoLog-iOS
+//
+//  Created by ✨EUGENE✨ on 2021/05/19.
+//
 
 import UIKit
 
-import SnapKit
+class HomeViewController: UIViewController {
 
-
-final class HomeViewController: UIViewController {
+    @IBOutlet weak var tomatoLogButton: UIButton!
+    @IBOutlet weak var settingButton: UIButton!
     
-    // TODO: 임시로 지정 해놓은 Label, 실제 화면이 추가되면 제거
-    private let homeLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Home"
-        label.textAlignment = .center
-        
-        return label
-    }()
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var tomatoImageView: UIImageView!
     
+    @IBOutlet weak var homeCollectionView: UICollectionView!
+    
+    // 임시로 이렇게 해두기
+    var buttonLabel = "유숩 님의 영화 로그\n전체보기"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.defineUI()
-        self.setConstraints()
+        setButton()
     }
     
-    private func defineUI() {
-        self.view.addSubview(self.homeLabel)
-        self.view.backgroundColor = .red
+    @IBAction func buttonTap(_ sender: Any) {
+        print("press success")
     }
     
-    private func setConstraints() {
-        self.homeLabel.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-        }
+    func setButton() {
+        // 토마토 로그
+        let boldFont = UIFont.boldSystemFont(ofSize: 18)
+        let smallFont = UIFont.systemFont(ofSize: 12)
+        
+        let attributedStr = NSMutableAttributedString(string: buttonLabel)
+        attributedStr.addAttribute(.font, value: boldFont, range: (buttonLabel as NSString).range(of: "유숩"))
+        attributedStr.addAttribute(.font, value: smallFont, range: (buttonLabel as NSString).range(of: "전체보기"))
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 11
+        attributedStr.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attributedStr.length))
+
+        tomatoLogButton.titleLabel?.numberOfLines = 2
+        tomatoLogButton.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        tomatoLogButton.titleLabel?.tintColor = .black
+        tomatoLogButton.setAttributedTitle(attributedStr, for: .normal)
+        
+        // 설정 버튼
+        settingButton.tintColor = .black
+        settingButton.setImage(R.image.btnSetting(), for: .normal)
     }
 }
